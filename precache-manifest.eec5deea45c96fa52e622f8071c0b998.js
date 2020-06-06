f26b64bb26da795f5e11c003d2c706865de77986
Sayendra Nadh Chowdary <sayendranadhchowdary@Sayendras-MacBook-Air.local>
2020-06-07 00:13:07 +0530

    Updates

---
+++ precache-manifest.eec5deea45c96fa52e622f8071c0b998.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77f1ca1e0173899d50e003c88138e709",
+    "url": "/satyaportfolio/index.html"
+  },
+  {
+    "revision": "2b607f5c913cfecd69bb",
+    "url": "/satyaportfolio/static/css/2.b1664065.chunk.css"
+  },
+  {
+    "revision": "fa64367d5ab1fea5692d",
+    "url": "/satyaportfolio/static/css/main.575ac8eb.chunk.css"
+  },
+  {
+    "revision": "2b607f5c913cfecd69bb",
+    "url": "/satyaportfolio/static/js/2.31a285a5.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/satyaportfolio/static/js/2.31a285a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa64367d5ab1fea5692d",
+    "url": "/satyaportfolio/static/js/main.a5502b6d.chunk.js"
+  },
+  {
+    "revision": "869b0b1d36c88e491960",
+    "url": "/satyaportfolio/static/js/runtime-main.3296e802.js"
+  },
+  {
+    "revision": "88eeb40123c3721d2ce5ea591fe896d5",
+    "url": "/satyaportfolio/static/media/api.88eeb401.svg"
+  },
+  {
+    "revision": "188f34f3f552ffac4e11a1cd0c09bf48",
+    "url": "/satyaportfolio/static/media/blog.188f34f3.webp"
+  },
+  {
+    "revision": "7033e4f29eba96221d98ce0b48b687db",
+    "url": "/satyaportfolio/static/media/bootstrap.7033e4f2.svg"
+  },
+  {
+    "revision": "45ff40946f8417c19448beb1bb4842f8",
+    "url": "/satyaportfolio/static/media/boston.45ff4094.png"
+  },
+  {
+    "revision": "dd86925b8b0c349cf2ca7d1dc0a92b56",
+    "url": "/satyaportfolio/static/media/codesandbox.dd86925b.png"
+  },
+  {
+    "revision": "e5f445d9a4e4a3a088dac639f3d7cc4a",
+    "url": "/satyaportfolio/static/media/coding2.e5f445d9.svg"
+  },
+  {
+    "revision": "5a75af113f40f14b7e09f0552dab811b",
+    "url": "/satyaportfolio/static/media/contact.5a75af11.jpg"
+  },
+  {
+    "revision": "078c6f9c664cb484ec1dfa8dc52fea98",
+    "url": "/satyaportfolio/static/media/css3.078c6f9c.svg"
+  },
+  {
+    "revision": "32497d5ccc92d78514117414481f1068",
+    "url": "/satyaportfolio/static/media/expressjs-icon.32497d5c.svg"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/satyaportfolio/static/media/github.76d344f0.svg"
+  },
+  {
+    "revision": "668edfb39633b98319456e128e811ea0",
+    "url": "/satyaportfolio/static/media/graduation.668edfb3.svg"
+  },
+  {
+    "revision": "983a23b3e7b3c4ac1b2c5c54c00e4a82",
+    "url": "/satyaportfolio/static/media/hotel.983a23b3.png"
+  },
+  {
+    "revision": "ea5191d678378e00b4283f0ca5209dda",
+    "url": "/satyaportfolio/static/media/html5-icon.ea5191d6.svg"
+  },
+  {
+    "revision": "9e5c4f89230472ee75a1d3f634a156c3",
+    "url": "/satyaportfolio/static/media/javascript.9e5c4f89.svg"
+  },
+  {
+    "revision": "d2ad77ab1e3dc3c67cd76abf4cad3528",
+    "url": "/satyaportfolio/static/media/library.d2ad77ab.png"
+  },
+  {
+    "revision": "67a0ad9f76f78ffe596a691205bcace0",
+    "url": "/satyaportfolio/static/media/mongodb-icon.67a0ad9f.svg"
+  },
+  {
+    "revision": "1933d4a0c800d2e34c7ca4736c20b619",
+    "url": "/satyaportfolio/static/media/mysql-icon.1933d4a0.svg"
+  },
+  {
+    "revision": "11b3e6ecc53e1d88d8a8413825e26dec",
+    "url": "/satyaportfolio/static/media/nodejs.11b3e6ec.svg"
+  },
+  {
+    "revision": "ee90f880851babd251563cc40094eef8",
+    "url": "/satyaportfolio/static/media/postman-icon.ee90f880.svg"
+  },
+  {
+    "revision": "9c62aba8285bcdab8197a31d213eeec7",
+    "url": "/satyaportfolio/static/media/react-icon.9c62aba8.svg"
+  },
+  {
+    "revision": "4230b7a02f0f90aaec8c3327ae018869",
+    "url": "/satyaportfolio/static/media/table.4230b7a0.svg"
+  },
+  {
+    "revision": "1453362e79ad8a64113cda0f14e2c7f1",
+    "url": "/satyaportfolio/static/media/translator.1453362e.jpeg"
+  },
+  {
+    "revision": "98ed01e18d26b89ee0d8edb277301472",
+    "url": "/satyaportfolio/static/media/work.98ed01e1.svg"
+  }
+]);
